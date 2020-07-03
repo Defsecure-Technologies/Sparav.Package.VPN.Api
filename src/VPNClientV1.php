fd8dfd4c585470988cf54d10bd4c2b2c0c20d60f
Blerim Cazimi <blerim@burd.dk>
2020-07-03 12:22:39 +0200

    Inital

---
+++ src/VPNClientV1.php
@@ -0,0 +1,29 @@
+<?php
+namespace Sparav\Vpn;
+
+
+class VPNClientV1
+{
+
+    /**
+     * @param string $type [openvpn etc]
+     * @param string $lang
+     * @param bool $region_ip
+     * @param string $device_id
+     * @return mixed
+     */
+    public function regions(string $type, string $lang, bool $region_ip, string $device_id)
+    {
+        $response = Http::timeout(15)
+            ->withBasicAuth(env('SPARAV_CUSTOMER_API_AUTH_USERNAME'), env('SPARAV_CUSTOMER_API_AUTH_PASSWORD'))
+            ->get('https://sparavvpnapiprod.azurewebsites.net/api/v1/regions',  [
+                'type' => $type,
+                'lang' => $lang,
+                'region_ip' => $region_ip,
+                'device_id' => $device_id
+            ]);
+        return $response;
+
+    }
+
+}
